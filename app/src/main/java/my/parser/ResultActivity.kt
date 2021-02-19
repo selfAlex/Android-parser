03a5ac6f830d4668da99c2b3643744870ce25fe1
selfAlex <alex20k.x@gmail.com>
2021-02-19 12:31:03 +0600

    Class for ResultActivity

---
+++ app/src/main/java/my/parser/ResultActivity.kt
@@ -0,0 +1,14 @@
+package my.parser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+    }
+
+}
