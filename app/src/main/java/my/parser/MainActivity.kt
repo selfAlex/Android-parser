f10ef6cf6e37ef79adebefe2ac6a599b9179591f
selfAlex <alex20k.x@gmail.com>
2021-02-19 12:36:15 +0600

    function for redirect

--- app/src/main/java/my/parser/MainActivity.kt
+++ app/src/main/java/my/parser/MainActivity.kt
@@ -3,9 +3,19 @@ package my.parser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.View
+import android.content.Intent
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun find(view: View) {
+        val intent = Intent(this, ResultActivity::class.java)
+
+        startActivity(intent)
+
     }
 }
