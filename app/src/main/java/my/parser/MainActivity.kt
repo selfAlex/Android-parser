cfc5ae6f7397f336c3fa48a0c7e464cc351f3707
selfAlex <alex20k.x@gmail.com>
2021-02-20 10:30:34 +0600

    get data from views

--- app/src/main/java/my/parser/MainActivity.kt
+++ app/src/main/java/my/parser/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 import android.view.View
 import android.content.Intent
+import android.widget.Spinner
+import android.widget.Switch
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,22 @@ class MainActivity : AppCompatActivity() {
 
     fun find(view: View) {
         val intent = Intent(this, ResultActivity::class.java)
+
+        val spinnerHardware: Spinner = findViewById(R.id.spinnerHardware)
+        val hardware = spinnerHardware.selectedItem.toString();
+        intent.putExtra("hardware", hardware)
+
+        val switchShop: Switch = findViewById(R.id.switchShop)
+        val useShop = switchShop.isChecked
+        intent.putExtra("useShop", useShop)
+
+        val switchTechnodom: Switch = findViewById(R.id.switchTechnodom)
+        val useTechnodom = switchTechnodom.isChecked
+        intent.putExtra("useTechnodom", useTechnodom)
+
+        val switchSatu : Switch = findViewById(R.id.switchSatu)
+        val useSatu = switchSatu.isChecked
+        intent.putExtra("useSatu", useSatu)
 
         startActivity(intent)
 
